a0c18c364bced45ab2019cf28e7f9b851427f125
anhnv33 <anhnv.it.91@gmail.com>
2019-04-04 22:41:52 +0700

    commit

--- authenticating-ldap/src/main/java/com/example/authenticatingldap/config/WebSecurityConfig.java
+++ authenticating-ldap/src/main/java/com/example/authenticatingldap/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.authenticatingldap;
+package com.example.authenticatingldap.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- authenticating-ldap/src/main/java/com/example/authenticatingldap/controller/HomeController.java
+++ authenticating-ldap/src/main/java/com/example/authenticatingldap/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.authenticatingldap;
+package com.example.authenticatingldap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
