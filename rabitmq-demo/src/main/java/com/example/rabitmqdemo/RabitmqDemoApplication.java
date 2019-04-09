c3f04de1f1094565ff244e229b4de0d1225f43fe
anhnv33 <anhnv.it.91@gmail.com>
2019-04-09 18:02:18 +0700

    commit

---
+++ rabitmq-demo/src/main/java/com/example/rabitmqdemo/RabitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitmqDemoApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(RabitmqDemoApplication.class, args);
+    }
+
+}
